e3ed74284e82462c7eade232d55a7e03c55d1cf5
Luis Alejandro Sanmiguel Galeano <134570324+AlejandroSanmiguel22@users.noreply.github.com>
2024-11-16 10:33:22 -0500

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'https://artesanosonline-backend.onrender.com'
+export const url = 'https://artesanosonline-backend-ak8f.onrender.com'
 export const currency = '$'
 
 export const assets ={
